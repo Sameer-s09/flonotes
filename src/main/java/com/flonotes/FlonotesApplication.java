c914b95a0296340b5cf783cca79ba225b520fb6f
sameerdjango <sameer.s09@gmail.com>
2022-03-28 22:43:19 +0530

    initial commit

---
+++ src/main/java/com/flonotes/FlonotesApplication.java
@@ -0,0 +1,13 @@
+package com.flonotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlonotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlonotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flonotes/FlonotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flonotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlonotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
